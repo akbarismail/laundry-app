79c98a1ffc3c5b57780db8b6f21e7e1002ee0299
akbarismail <ismailakbar.me@gmail.com>
2023-09-01 10:11:58 +0700

    [FEATURE] add config

--- config/config.go
+++ config/config.go
@@ -30,7 +30,7 @@ func (c *Config) ReadConfig() error {
 
 	if c.DbConfig.Host == "" || c.DbConfig.Port == "" || c.DbConfig.Name == "" || c.DbConfig.User == "" || c.DbConfig.Password == "" || c.DbConfig.Driver == "" {
 
-		fmt.Errorf("Missing required environment variable")
+		return fmt.Errorf("missing required environment variable")
 	}
 
 	return nil
