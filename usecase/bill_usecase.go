38a325f58fc5a2ec350a55746583c9c34680c7ea
akbarismail <ismailakbar.me@gmail.com>
2023-09-03 21:55:16 +0700

    [IMPROVE] completed func bill controller

--- usecase/bill_usecase.go
+++ usecase/bill_usecase.go
@@ -7,9 +7,9 @@ import (
 )
 
 type BillUseCase interface {
-	CreateNew(payload model.Bill) error
-	GetAll() ([]model.Bill, error)
-	GetById(id string) (model.Bill, error)
+	CreateNew(payloadBill model.Bill, payloadBillDetail model.BillDetail) error
+	GetAll() ([]model.Bill, []model.BillDetail, error)
+	GetById(id string) (model.Bill, []model.BillDetail, error)
 }
 
 type billUseCase struct {
@@ -20,39 +20,88 @@ type billUseCase struct {
 }
 
 // GetById implements BillUseCase.
-func (b *billUseCase) GetById(id string) (model.Bill, error) {
-	panic("unimplemented")
+func (b *billUseCase) GetById(id string) (model.Bill, []model.BillDetail, error) {
+	bill, billsDetail, err := b.repo.FindById(id)
+	if err != nil {
+		return bill, nil, fmt.Errorf("failed to find bill: %v", err)
+	}
+
+	return bill, billsDetail, nil
 }
 
 // GetAll implements BillUseCase.
-func (b *billUseCase) GetAll() ([]model.Bill, error) {
-	panic("unimplemented")
+func (b *billUseCase) GetAll() ([]model.Bill, []model.BillDetail, error) {
+	bills, billsDetail, err := b.repo.FindByAll()
+	if err != nil {
+		return nil, nil, fmt.Errorf("data is empty")
+	}
+
+	return bills, billsDetail, nil
 }
 
 // CreateNew implements BillUseCase.
-func (b *billUseCase) CreateNew(payload model.Bill) error {
-	if payload.ID == "" {
+func (b *billUseCase) CreateNew(payloadBill model.Bill, payloadBillDetail model.BillDetail) error {
+	if payloadBill.ID == "" {
 		return fmt.Errorf("id is required")
 	}
 
-	_, err := b.employeeUC.GetById(payload.Employee.ID)
+	if payloadBill.BillDate.String() == "" {
+		return fmt.Errorf("bill date is required")
+	}
+
+	if payloadBill.EntryDate.String() == "" {
+		return fmt.Errorf("entry date is required")
+	}
+
+	if payloadBill.FinishDate.String() == "" {
+		return fmt.Errorf("finish date is required")
+	}
+
+	if payloadBill.Employee.ID == "" {
+		return fmt.Errorf("employee id is required")
+	}
+
+	if payloadBill.Customer.ID == "" {
+		return fmt.Errorf("customer id is required")
+	}
+
+	_, err := b.employeeUC.GetById(payloadBill.Employee.ID)
 	if err != nil {
 		return fmt.Errorf("employee is not found")
 	}
 
-	_, err = b.customerUC.GetById(payload.Customer.ID)
+	_, err = b.customerUC.GetById(payloadBill.Customer.ID)
 	if err != nil {
 		return fmt.Errorf("customer is not found")
 	}
 
-	for _, billDetail := range payload.BillDetail {
-		_, err = b.productUC.GetById(billDetail.Product.ID)
-		if err != nil {
-			return fmt.Errorf("product is not found")
-		}
+	if payloadBillDetail.ID == "" {
+		return fmt.Errorf("bill detail id is required")
 	}
 
-	err = b.repo.Save(payload)
+	if payloadBillDetail.Bill.ID == "" {
+		return fmt.Errorf("bill id is required")
+	}
+
+	// _, err = b.repo.FindById(payloadBillDetail.Bill.ID)
+	// if err != nil {
+	// 	return fmt.Errorf("bill is not found")
+	// }
+
+	_, err = b.productUC.GetById(payloadBillDetail.Product.ID)
+	if err != nil {
+		return fmt.Errorf("product is not found")
+	}
+
+	if payloadBillDetail.ProductPrice <= 0 {
+		return fmt.Errorf("product price is must be greater than zero")
+	}
+
+	if payloadBillDetail.Qty <= 0 {
+		return fmt.Errorf("qty is must be greater than zero")
+	}
+
+	err = b.repo.Save(payloadBill, payloadBillDetail)
 	if err != nil {
 		return fmt.Errorf("failed to create bill: %v", err)
 	}
