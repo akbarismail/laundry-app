e67a6599d8233c9f86e9cfd405f29021108774f4
akbarismail <ismailakbar.me@gmail.com>
2023-09-03 21:54:31 +0700

    [IMPROVE] completed function bill repository

--- repository/bill_repository.go
+++ repository/bill_repository.go
@@ -6,9 +6,9 @@ import (
 )
 
 type BillRepository interface {
-	Save(bill model.Bill) error
-	FindByAll() ([]model.Bill, error)
-	FindById(id string) (model.Bill, error)
+	Save(bill model.Bill, billDetail model.BillDetail) error
+	FindByAll() ([]model.Bill, []model.BillDetail, error)
+	FindById(id string) (model.Bill, []model.BillDetail, error)
 }
 
 type billRepository struct {
@@ -16,42 +16,142 @@ type billRepository struct {
 }
 
 // FindById implements BillRepository.
-func (b *billRepository) FindById(id string) (model.Bill, error) {
-	r := b.db.QueryRow("SELECT b.id, b.bill_date, b.entry_date, b.finish_date, e.id, e.name, e.phone_number, e.address, c.id, c.name, c.phone_number, c.address FROM bill AS b JOIN employee AS e ON e.id = b.employee_id JOIN customer AS c ON c.id = b.customer_id WHERE b.id=$1;", id)
+func (b *billRepository) FindById(id string) (model.Bill, []model.BillDetail, error) {
+	tx, err := b.db.Begin()
+	if err != nil {
+		return model.Bill{}, nil, err
+	}
+
+	r := tx.QueryRow("SELECT b.id, b.bill_date, b.entry_date, b.finish_date, e.id, e.name, e.phone_number, e.address, c.id, c.name, c.phone_number, c.address FROM bill AS b JOIN employee AS e ON e.id = b.employee_id JOIN customer AS c ON c.id = b.customer_id WHERE b.id=$1;", id)
 
 	bill := model.Bill{}
-	err := r.Scan(&bill.ID, &bill.BillDate, &bill.EntryDate, &bill.FinishDate, &bill.Employee.ID, &bill.Employee.Name, &bill.Employee.PhoneNumber, &bill.Employee.Address, &bill.Customer.ID, &bill.Customer.Name, &bill.Customer.PhoneNumber, &bill.Customer.Address)
+	err = r.Scan(&bill.ID, &bill.BillDate, &bill.EntryDate, &bill.FinishDate, &bill.Employee.ID, &bill.Employee.Name, &bill.Employee.PhoneNumber, &bill.Employee.Address, &bill.Customer.ID, &bill.Customer.Name, &bill.Customer.PhoneNumber, &bill.Customer.Address)
 	if err != nil {
-		return bill, err
+		err = tx.Rollback()
+		if err != nil {
+			return bill, nil, err
+		}
+
+		return bill, nil, err
 	}
-	return bill, nil
+
+	r2, err := tx.Query("SELECT bd.id, bd.product_price, bd.qty, b.id, b.bill_date, b.entry_date, b.finish_date, p.id, p.name, p.price, u.id, u.name FROM bill_detail AS bd JOIN bill AS b ON b.id=bd.bill_id JOIN product AS p ON p.id=bd.product_id JOIN uom as u ON u.id=p.uom_id WHERE bd.bill_id=$1;", id)
+	if err != nil {
+		err = tx.Rollback()
+		if err != nil {
+			return bill, nil, err
+		}
+
+		return bill, nil, err
+	}
+
+	defer r2.Close()
+	billsDetail := []model.BillDetail{}
+	billDetail := model.BillDetail{}
+	for r2.Next() {
+		err = r2.Scan(&billDetail.ID, &billDetail.ProductPrice, &billDetail.Qty, &billDetail.Bill.ID, &billDetail.Bill.BillDate, &billDetail.Bill.EntryDate, &billDetail.Bill.FinishDate, &billDetail.Product.ID, &billDetail.Product.Name, &billDetail.Product.Price, &billDetail.Product.Uom.ID, &billDetail.Product.Uom.Name)
+		if err != nil {
+			return bill, nil, err
+		}
+
+		billsDetail = append(billsDetail, billDetail)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return bill, nil, err
+	}
+
+	return bill, billsDetail, nil
 }
 
 // FindByAll implements BillRepository.
-func (b *billRepository) FindByAll() ([]model.Bill, error) {
-	r, err := b.db.Query("SELECT b.id, b.bill_date, b.entry_date, b.finish_date, e.id, e.name, e.phone_number, e.address, c.id, c.name, c.phone_number, c.address FROM bill AS b JOIN employee AS e ON e.id = b.employee_id JOIN customer AS c ON c.id = b.customer_id;")
+func (b *billRepository) FindByAll() ([]model.Bill, []model.BillDetail, error) {
+	tx, err := b.db.Begin()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
+	}
+
+	r, err := tx.Query("SELECT b.id, b.bill_date, b.entry_date, b.finish_date, e.id, e.name, e.phone_number, e.address, c.id, c.name, c.phone_number, c.address FROM bill AS b JOIN employee AS e ON e.id = b.employee_id JOIN customer AS c ON c.id = b.customer_id;")
+	if err != nil {
+		err = tx.Rollback()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		return nil, nil, err
 	}
 
 	defer r.Close()
 	bills := []model.Bill{}
+	bill := model.Bill{}
 
 	for r.Next() {
-		bill := model.Bill{}
 		err = r.Scan(&bill.ID, &bill.BillDate, &bill.EntryDate, &bill.FinishDate, &bill.Employee.ID, &bill.Employee.Name, &bill.Employee.PhoneNumber, &bill.Employee.Address, &bill.Customer.ID, &bill.Customer.Name, &bill.Customer.PhoneNumber, &bill.Customer.Address)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		bills = append(bills, bill)
 	}
 
-	return bills, nil
+	r2, err := tx.Query("SELECT bd.id, bd.product_price, bd.qty, b.id, b.bill_date, b.entry_date, b.finish_date, p.id, p.name, p.price, u.id, u.name FROM bill_detail AS bd JOIN bill AS b ON b.id=bd.bill_id JOIN product AS p ON p.id=bd.product_id JOIN uom as u ON u.id=p.uom_id;")
+	if err != nil {
+		err = tx.Rollback()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		return nil, nil, err
+	}
+
+	defer r2.Close()
+	billsDetail := []model.BillDetail{}
+	billDetail := model.BillDetail{}
+	for r2.Next() {
+		err = r2.Scan(&billDetail.ID, &billDetail.ProductPrice, &billDetail.Qty, &billDetail.Bill.ID, &billDetail.Bill.BillDate, &billDetail.Bill.EntryDate, &billDetail.Bill.FinishDate, &billDetail.Product.ID, &billDetail.Product.Name, &billDetail.Product.Price, &billDetail.Product.Uom.ID, &billDetail.Product.Uom.Name)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		billsDetail = append(billsDetail, billDetail)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return bills, billsDetail, nil
 }
 
 // Save implements BillRepository.
-func (b *billRepository) Save(bill model.Bill) error {
-	_, err := b.db.Exec("INSERT INTO bill VALUES($1, $2, $3, $4, $5, $6);", bill.ID, bill.BillDate, bill.EntryDate, bill.FinishDate, bill.Employee.ID, bill.Customer.ID)
+func (b *billRepository) Save(bill model.Bill, billDetail model.BillDetail) error {
+	tx, err := b.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("INSERT INTO bill VALUES($1, $2, $3, $4, $5, $6);", bill.ID, bill.BillDate, bill.EntryDate, bill.FinishDate, bill.Employee.ID, bill.Customer.ID)
+	if err != nil {
+		err = tx.Rollback()
+		if err != nil {
+			return err
+		}
+
+		return err
+	}
+
+	_, err = tx.Exec("INSERT INTO bill_detail VALUES($1, $2, $3, $4, $5);", billDetail.ID, billDetail.Bill.ID, billDetail.Product.ID, billDetail.ProductPrice, billDetail.Qty)
+	if err != nil {
+		err = tx.Rollback()
+		if err != nil {
+			return err
+		}
+
+		return err
+	}
+
+	err = tx.Commit()
 	if err != nil {
 		return err
 	}
@@ -60,5 +160,7 @@ func (b *billRepository) Save(bill model.Bill) error {
 }
 
 func NewBillRepository(db *sql.DB) BillRepository {
-	return &billRepository{db: db}
+	return &billRepository{
+		db: db,
+	}
 }
